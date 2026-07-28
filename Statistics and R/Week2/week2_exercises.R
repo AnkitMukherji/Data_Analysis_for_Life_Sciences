c1c7792d8b0d7c7257b9de0163ac39504e6ce147
Ankit Mukherjee <ankitmukherjee@Ankits-MacBook-Pro.local>
2026-07-28 22:57:51 +0530

    clt and t dist

--- Statistics and R/Week2/week2_exercises.R
+++ Statistics and R/Week2/week2_exercises.R
@@ -249,3 +249,97 @@ Y <- sample(y, 25)
 
 abs((mean(y) - mean(x)) - (mean(Y) - mean(X)))
 # 0.3647172
+
+############################# Central Limit Theorem Exercises #############################
+dat <- na.omit(read.csv("mice_pheno.csv"))
+head(dat)
+
+## Central Limit Theorem Exercises #1
+# If a list of numbers has a distribution that is well approximated by the normal distribution, 
+# what proportion of these numbers are within one standard deviation away from the list's average?
+# Use the pnorm() function. You can look up more information with ?pnorm.
+pnorm(1) - pnorm(-1)
+# 0
+
+## Central Limit Theorem Exercises #2
+# What proportion of these numbers are within two standard deviations away from the list's average?
+pnorm(2) - pnorm(-2)
+# 0.9544997
+
+## Central Limit Theorem Exercises #3
+# What proportion of these numbers are within three standard deviations away from the list's average?
+pnorm(3) - pnorm(-3)
+# 0.9973002
+
+## Central Limit Theorem Exercises #4
+# Define y to be the weights of males on the control diet.
+
+# What proportion of the mice are within one standard deviation away from the average weight?
+# Remember to use popsd() from rafalib for the population standard deviation.
+library(tidyverse)
+library(rafalib)
+
+y <- dat |> 
+  filter(Sex == "M" & Diet == "chow") |> 
+  pull(Bodyweight)
+
+z <- (y - mean(y)) / popsd(y)
+mean(abs(z) <= 1)
+# 0.6950673
+
+## Central Limit Theorem Exercises #5
+# What proportion of these numbers are within two standard deviations away from the list's average?
+mean(abs(z) <= 2)
+# 0.9461883
+
+## Central Limit Theorem Exercises #6
+# What proportion of these numbers are within three standard deviations away from the list's average?
+mean(abs(z) <= 3)
+# 0.9910314
+
+## Central Limit Theorem Exercised #7
+# Note that the numbers for the normal distribution and our weights are relatively close. 
+# Also, notice that we are indirectly comparing quantiles of the normal distribution to quantiles 
+# of the mouse weight distribution. We can actually compare all quantiles using a qqplot.
+qqnorm(z)
+abline(0,1)
+# Which of the following best describes the qq-plot comparing mouse weights to the normal distribution?
+
+# The mouse weights are well approximated by the normal distribution, 
+# although the larger values (right tail) are larger than predicted by the normal. 
+# This is consistent with the differences seen between question 3 and 6.
+
+## Central Limit Theorem Exercises #8
+# Here we are going to use the function replicate() to learn about the distribution of random variables. 
+# All the above exercises relate to the normal distribution as an approximation of the distribution 
+# of a fixed list of numbers or a population. We have not yet discussed probability in these exercises. 
+# If the distribution of a list of numbers is approximately normal, then if we pick a number at random 
+# from this distribution, it will follow a normal distribution. However, it is important to remember 
+# that stating that some quantity has a distribution does not necessarily imply this quantity is random. 
+# Also, keep in mind that this is not related to the central limit theorem. 
+# The central limit applies to averages of random variables. Let's explore this concept.
+
+# We will now take a sample of size 25 from the population of males on the chow diet. 
+# The average of this sample is our random variable. 
+# We will use the replicate() function to observe 10,000 realizations of this random variable. 
+# Set the seed at 1, then generate these 10,000 averages. 
+# Make a histogram and qq-plot of these 10,000 numbers against the normal distribution.
+
+# We can see that, as predicted by the CLT, the distribution of the random variable is very well 
+# approximated by the normal distribution.
+y <- filter(dat, Sex=="M" & Diet=="chow") %>% select(Bodyweight) %>% unlist
+set.seed(1)
+avgs <- replicate(10000, mean( sample(y, 25)))
+mypar(1,2)
+hist(avgs)
+qqnorm(avgs)
+qqline(avgs)
+
+# What is the average of the distribution of the sample average?
+mean(avgs)
+# 30.96856
+
+## Central Limit Theorem Exercises #9
+# What is the standard deviation of the distribution of sample averages (use popsd())?
+popsd(avgs)
+# 0.827082
